e04aa2538779d8f5dcbe608d80f4a75f49b11ae3
Shimobouzi <e1b23037@oit.ac.jp>
2025-11-11 15:29:03 +0900

    spring_boot追加

---
+++ nats/src/main/java/team2/nats/NatsApplication.java
@@ -0,0 +1,13 @@
+package team2.nats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatsApplication.class, args);
+	}
+
+}
