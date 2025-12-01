3bd955d6c0253fc5317a413684b59b7cc62c0912
NakamuraKenshiro <e1b23061@oit.ac.jp>
2025-12-01 11:52:11 +0900

    回答フォームを表示するgame.htmlを作成する

--- nats/src/main/java/team2/nats/controller/GameController.java
+++ nats/src/main/java/team2/nats/controller/GameController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import team2.nats.repository.ImageRepository;
 
@@ -18,6 +20,13 @@ public class GameController {
   @GetMapping("")
   public String list(ModelMap model) {
     model.addAttribute("images", imageRepository.findAll());
-    return "game.html";
+    return "game"; // templates/game.html
+  }
+
+  @PostMapping("/answer")
+  public String answer(@RequestParam("answer") String answer, ModelMap model) {
+    model.addAttribute("images", imageRepository.findAll());
+    model.addAttribute("message", "回答を受け付けました: " + answer);
+    return "game";
   }
 }
